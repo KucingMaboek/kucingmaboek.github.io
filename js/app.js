78573ab428d5d8fe19526c82a31bc68fde18c4bc
KucingMaboek <kucingmaboek.iam@gmail.com>
2024-07-28 19:54:47 +0700

    - remove header logo - change header name and role - add download resume button in header - adjust project column to 3 item per row - fix education h2 - seperate work/education date to new line - add read more/less function for work experience description - create carousel slider function for certification section

---
+++ js/app.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const carousel = document.querySelector(".certification-carousel");
+  const wrapper = document.querySelector(".certification-wrapper");
+
+  let isDragging = false,
+    startX,
+    startScrollLeft,
+    timeoutId;
+
+  const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+  };
+
+  const dragging = (e) => {
+    if (!isDragging) return;
+
+    // Calculate the new scroll position
+    const newScrollLeft = startScrollLeft - (e.pageX - startX);
+
+    // Check if the new scroll position exceeds
+    // the carousel boundaries
+    if (newScrollLeft <= 0 || newScrollLeft >=
+      carousel.scrollWidth - carousel.offsetWidth) {
+
+      // If so, prevent further dragging
+      isDragging = false;
+      return;
+    }
+
+    // Otherwise, update the scroll position of the carousel
+    carousel.scrollLeft = newScrollLeft;
+  };
+
+  const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+  };
+
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragStop);
+  wrapper.addEventListener("mouseenter", () =>
+    clearTimeout(timeoutId));
+});
+
+$(document).ready(function() {
+  (function() {
+    var ellipsestext = "...";
+    var showChar;
+
+    if (window.matchMedia("(max-width: 764px)").matches) {
+      showChar = 50;
+    } else {
+      showChar = 200;
+    }
+
+    $(".experience-description").each(function() {
+      var content = $(this).html();
+      if (content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content;
+        var html =
+          '<div class="truncate-text" style="display:block">' +
+          c +
+          '<span class="moreellipses">' +
+          ellipsestext +
+          '&nbsp;&nbsp;<a href="" class="moreless more">more</a></span></span></div><div class="truncate-text" style="display:none">' +
+          h +
+          '<a href="" class="moreless less">less</a></span></div>';
+
+        $(this).html(html);
+      }
+    });
+
+    $(".moreless").click(function() {
+      var thisEl = $(this);
+      var cT = thisEl.closest(".truncate-text");
+      var tX = ".truncate-text";
+
+      if (thisEl.hasClass("less")) {
+        cT.prev(tX).toggle();
+        cT.toggle();
+      } else {
+        cT.toggle();
+        cT.next(tX).toggle();
+      }
+      return false;
+    });
+  })();
+});
